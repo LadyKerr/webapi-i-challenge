5768a429c944317af9f7220ba9c8d58e7ef35d66
LadyKerr <kadiekerr@gmail.com>
2019-07-08 16:03:23 -0400

    DELETE request & working on POST conditional

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ server.use(express.json());
 
 //C-R-U-D
 
-//Read - show the array of users
+//Read - display the array of users
 server.get("/api/users", (req, res) => {
   Data.find()
     .then(user => {
@@ -28,7 +28,7 @@ server.get("/api/users/:id", (req, res) => {
   Data.findById(id)
     .then(user => {
       if (user) {
-        res.status(204).end();
+        res.status(200).json(user);
       } else {
         res
           .status(404)
@@ -39,6 +39,47 @@ server.get("/api/users/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+//Create
+server.post("/api/users", (req, res) => {
+  const userData = req.body;
+
+  Data.insert(userData)
+    .then(user => {
+      if (user) {
+        res.status(201).json(user);
+      } else {
+        res
+          .status(400)
+          .json({ errorMessage: "Please Provide name and bio for user" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+//Delete
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  Data.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res.status(204).end();
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ err: "The user could not be removed" });
+    });
+});
+
+// //Update
+// server.put("/", (req, res) => {});
 
 const port = 5000;
 server.listen(port, () =>
